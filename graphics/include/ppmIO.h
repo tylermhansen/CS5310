140db47fd838d750564137ed8cb3bc5379d7077b
Tyler Hansen <tyler.hansen@colby.edu>
2021-09-15 15:23:03 -0400

    Finish up lab1, generate some images for contrast filtering + user input extension

--- graphics/include/ppmIO.h
+++ graphics/include/ppmIO.h
@@ -14,5 +14,8 @@ void writePPM(Pixel *image, int rows, int cols, int colors, char *filename);
 unsigned char *readPGM(int *rows, int *cols, int *intensities, char *filename);
 void writePGM(unsigned char *image, long rows, long cols, int intensities, char *filename);
 
+int get_average_brightness(Pixel* image, int imagesize);
+Pixel* apply_contrast_filtering(Pixel* image, int imagesize, int average_brightness, double alpha);
+
 
 #endif
